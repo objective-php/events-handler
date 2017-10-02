6232de269b588f2a85bfd961ba7508f132367d9b
Vincent Simonin <vsi@opcoding.eu>
2017-10-02 17:38:57 +0200

    Add proxy method to EventsHandlerInterface method into EventHandlerAwareTrait

--- src/EventsHandlerAwareTrait.php
+++ src/EventsHandlerAwareTrait.php
@@ -37,4 +37,33 @@ trait EventsHandlerAwareTrait
 
         return $this;
     }
+
+    /**
+     * Proxy trigger method
+     *
+     * @param string              $eventName
+     * @param mixed               $origin
+     * @param array               $context
+     * @param EventInterface|null $event
+     */
+    public function trigger($eventName, $origin = null, $context = [], EventInterface $event = null)
+    {
+        if ($eventsHandler = $this->getEventsHandler()) {
+            $eventsHandler->trigger($eventName, $origin, $context, $event);
+        }
+    }
+
+    /**
+     * Proxy bind method
+     *
+     * @param string $eventName
+     * @param string $callback
+     * @param string $mode
+     */
+    public function bind($eventName, $callback, $mode = EventsHandlerInterface::BINDING_MODE_LAST)
+    {
+        if ($eventsHandler = $this->getEventsHandler()) {
+            $eventsHandler->bind($eventName, $callback, $mode);
+        }
+    }
 }
---
+++ tests/Events/EventsHandlerAwareTraitTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\ObjectivePHP\Events;
+
+use ObjectivePHP\Events\EventsHandler;
+use ObjectivePHP\Events\EventsHandlerAwareTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class EventsHandlerAwareTraitTest
+ *
+ * @package Tests\ObjectivePHP\Events
+ */
+class EventsHandlerAwareTraitTest extends TestCase
+{
+    public function testEventsHandlerAccessors()
+    {
+        $instance = new class {
+            use EventsHandlerAwareTrait;
+        };
+
+        $eventsHandler = new EventsHandler();
+
+        $instance->setEventsHandler($eventsHandler);
+
+        $this->assertEquals($eventsHandler, $instance->getEventsHandler());
+        $this->assertAttributeEquals($instance->getEventsHandler(), 'eventsHandler', $instance);
+    }
+
+    public function testEventIsTriggered()
+    {
+        $instance = new class {
+            use EventsHandlerAwareTrait;
+        };
+
+        $eventName = 'event.test';
+        $origin = $this;
+
+        $eventsHandler = $this->getMockBuilder(EventsHandler::class)->getMock();
+        $eventsHandler->expects($this->once())->method('trigger')->with($eventName, $origin);
+
+        $instance->setEventsHandler($eventsHandler);
+
+        $instance->trigger($eventName, $this);
+    }
+
+    public function testEventIsBound()
+    {
+        $instance = new class {
+            use EventsHandlerAwareTrait;
+        };
+
+        $eventName = 'event.test';
+        $callback = function () {};
+
+        $eventsHandler = $this->getMockBuilder(EventsHandler::class)->getMock();
+        $eventsHandler->expects($this->once())->method('bind')->with($eventName, $callback);
+
+        $instance->setEventsHandler($eventsHandler);
+
+        $instance->bind($eventName, $callback);
+    }
+
+    public function testEventIsNotTriggered()
+    {
+        $instance = new class {
+            use EventsHandlerAwareTrait;
+        };
+
+        $eventsHandler = $this->getMockBuilder(EventsHandler::class)->getMock();
+        $eventsHandler->expects($this->never())->method('trigger');
+
+        $instance->trigger('test', $this);
+    }
+
+    public function testEventIsNotBound()
+    {
+        $instance = new class {
+            use EventsHandlerAwareTrait;
+        };
+
+        $eventsHandler = $this->getMockBuilder(EventsHandler::class)->getMock();
+        $eventsHandler->expects($this->never())->method('bind');
+
+        $instance->bind('test', function () {});
+    }
+}

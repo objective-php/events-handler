443a2549093318904058a498c05892e90bf2b9df
Vincent Simonin <vsi@opcoding.eu>
2017-10-02 16:06:18 +0200

    Fix typo in EventHandlerAware interface

--- src/EventsHandlerAwareInterface.php
+++ src/EventsHandlerAwareInterface.php
@@ -23,5 +23,5 @@ interface EventsHandlerAwareInterface
      *
      * @return $this
      */
-    public function setEventHandler(EventsHandlerInterface $eventsHandler);
+    public function setEventsHandler(EventsHandlerInterface $eventsHandler);
 }
